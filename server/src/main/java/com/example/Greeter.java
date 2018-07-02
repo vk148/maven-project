d1b1f92b7be113b117d1f5e2213b55667e264045
Vijay Kumar <vk3768@att.com>
2018-07-02 10:38:13 +0530

    fixed checkstyple errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,11 @@ public class Greeter {
   }
 
   /**
-  *@param someone name of 
-  *@return greeting string
+  *@param someone name of.
+  *@return greeting string.
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
+
