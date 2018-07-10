4cd2b2e7d52aef0231374c142d94cd4aada4e4d6
Vijay Kumar <vk3768@att.com>
2018-07-11 04:11:55 +0530

    test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,6 +6,10 @@ public class Greeter {
 
   }
 
+  /**
+  *@param someone name of.
+  *@return greeting string.
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
