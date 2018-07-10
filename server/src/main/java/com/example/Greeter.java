7aaf5bea08c331a8ad09f2ae309cf1fcb09123df
Vijay Kumar <vk3768@att.com>
2018-07-11 04:08:35 +0530

    test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,21 +1,11 @@
 package com.example;
 
-/**
- * This is a class.
- */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
   public Greeter() {
 
   }
 
-  /**
-  *@param someone name of.
-  *@return greeting string.
-  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
